e5c8c355942b5655ca225bcd0f9b107c648f7409
flumpyz <natalianata2001@mail.ru>
2022-04-06 10:31:27 +0500

    implemented page layout

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -64,6 +64,16 @@ class SiteController extends Controller
         return $this->render('index');
     }
 
+    public function actionView()
+    {
+        return $this->render('post');
+    }
+
+    public function actionCategory()
+    {
+        return $this->render('category');
+    }
+
     /**
      * Login action.
      *
